a1617845e8ad3bed1f7a9ee35056de77cb4279c9
AleksanderBartoszek <olekbartoszek2711@gmail.com>
2022-05-23 18:39:32 +0200

    version 1.0

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,5 +1,12 @@
 import akka.actor.{ActorSystem, Props}
 
+/** Program entry point
+ *
+ * @param in path to folder with images to process
+ * @param out path where to save processed images
+ * @param threshold  minimal score for discarding image as too dark
+ * @param func type of function used to calculate how bright the image is
+ */
 @main
 def ParallelDarkImages(in: String, out: String, threshold: Int, func: String): Unit = {
   val system = ActorSystem("system")
